dc9028b0e57a8727b209fc9ab7bcee6e1e5c9c9d
aakash <Aakash.Kumar@colorado.edu>
2017-08-24 19:28:57 -0600

    added cleanup, continuous blink

--- base.py
+++ base.py
@@ -5,8 +5,14 @@ print(gpio.RPI_INFO)
 
 gpio.setmode(gpio.BOARD)
 gpio.setup(7,gpio.OUT,initial=gpio.HIGH)
-time.sleep(1)
-print("High now")
-gpio.output(7,0)
-time.sleep(1)
-print("low")
+
+input(a)
+
+while(1):
+	time.sleep(1)
+	print("High now")
+	gpio.output(7,0)
+	time.sleep(1)
+	print("low")
+
+gpio.cleanup()
