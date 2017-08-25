80ce53da23982f2d43903dfba8908fe6ec39e3c1
aakash <Aakash.Kumar@colorado.edu>
2017-08-24 19:39:26 -0600

    working blink code with no of times and freq input

--- base.py
+++ base.py
@@ -6,14 +6,15 @@ print(gpio.RPI_INFO)
 gpio.setmode(gpio.BOARD)
 gpio.setup(7,gpio.OUT,initial=gpio.HIGH)
 
-a=raw_input("Enter no of times")
+num=raw_input("Enter no of times ")
+freq=raw_input("Enter on/off time ")
 
-for i in range (0,int(a)):
+for i in range (0,int(num)):
 	gpio.output(7,1)
-	time.sleep(1)
+	time.sleep(float(freq))
 	print("High now")
 	gpio.output(7,0)
-	time.sleep(1)
+	time.sleep(float(freq))
 	print("low")
 
 gpio.cleanup()
