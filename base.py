326f5ffe0891527234a6ead18a6c23c048546364
aakash <Aakash.Kumar@colorado.edu>
2017-08-24 19:19:50 -0600

    base code for pin 4

--- base.py
+++ base.py
@@ -1,1 +1,12 @@
-print("hello world")
+import RPi.GPIO as gpio
+import time
+
+print(gpio.RPI_INFO)
+
+gpio.setmode(gpio.BOARD)
+gpio.setup(4,gpio.OUT,initial=GPIO.HIGH)
+time.sleep(1)
+print("High now")
+gpio.output(4,0)
+time.sleep(1)
+print("low")
